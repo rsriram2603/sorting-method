9b3005f0c2f6aa975ac51350733349fc0b569ee3
rsriram2603 <rsriram830@gmail.com>
2023-03-24 17:11:25 +0530

    new file add

---
+++ main.js
@@ -0,0 +1,59 @@
+library = [
+
+{
+
+title: 'The Road Ahead',
+
+author: 'Bill Gates',
+
+libraryID: 1254
+
+},
+
+{
+
+title: 'Walter Isaacson',
+
+author: 'Steve Jobs',
+
+libraryID: 4264
+
+},
+
+{
+
+title: 'Mockingjay: The Final Book of The Hunger Games',
+
+author: 'Suzanne Collins',
+
+libraryID: 3245
+
+},
+
+{
+
+title: 'Adventures of Tom Sawyer',
+
+author: 'Mark Twain',
+
+libraryID: 1345
+
+},
+
+{
+
+title: 'Adventures of Sherlock Holmes',
+
+author: 'Sir Arthur Conan Doyle',
+
+libraryID: 2245
+
+}
+
+];
+var a=library.sort(function(e,b)
+{
+	return e.libraryID-b.libraryID;
+});
+console.log(a)
+	
